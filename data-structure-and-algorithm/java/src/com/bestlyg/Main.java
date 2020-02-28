df7799ee01e5cba3e631ee56eb2e5b7dddebe568
bestlyg <1057966749@qq.com>
2020-02-28 21:09:39 +0800

    feat: 🎸 add Set

--- data-structure-and-algorithm/java/src/com/bestlyg/Main.java
+++ data-structure-and-algorithm/java/src/com/bestlyg/Main.java
@@ -1,45 +1,81 @@
 package com.bestlyg;
 
-import com.bestlyg.tree.RBTree;
-import com.bestlyg.tree.printer.BinaryTrees;
+import com.bestlyg.set.ListSet;
+import com.bestlyg.set.Set;
+import com.bestlyg.set.Set.Visitor;
+import com.bestlyg.set.TreeSet;
+import com.bestlyg.utils.Times;
+import com.bestlyg.utils.Times.Task;
+import com.bestlyg.utils.file.FileInfo;
+import com.bestlyg.utils.file.Files;
 
 public class Main {
+	
+	static void test1() {
+
+		Set<Integer> listSet = new ListSet<>();
+		listSet.add(10);
+		listSet.add(11);
+		listSet.add(11);
+		listSet.add(12);
+		listSet.add(10);
+		
+		Set<Integer> treeSet = new TreeSet<>();
+		treeSet.add(12);
+		treeSet.add(10);
+		treeSet.add(7);
+		treeSet.add(11);
+		treeSet.add(10);
+		treeSet.add(11);
+		treeSet.add(9);
+		
+		treeSet.traversal(new Visitor<Integer>() {
+			@Override
+			public boolean visit(Integer element) {
+				System.out.println(element);
+				return false;
+			}
+		});
+	}
+	
+	
+	static void testSet(Set<String> set, String[] words) {
+		for (int i = 0; i < words.length; i++) {
+			set.add(words[i]);
+		}
+		for (int i = 0; i < words.length; i++) {
+			set.contains(words[i]);
+		}
+		for (int i = 0; i < words.length; i++) {
+			set.remove(words[i]);
+		}
+	}
+	
+	static void test2() {
+		FileInfo fileInfo = Files.read("D:\\soft\\jdk1.8\\src", 
+				new String[]{"java"});
+		
+		System.out.println("文件数量：" + fileInfo.getFiles());
+		System.out.println("代码行数：" + fileInfo.getLines());
+		String[] words = fileInfo.words();
+		System.out.println("单词数量：" + words.length);
+
+		Times.test("TreeSet", new Task() {
+			public void execute() {
+				testSet(new TreeSet<>(), words);
+			}
+		});
+		
+//		Times.test("ListSet", new Task() {
+//			public void execute() {
+//				testSet(new ListSet<>(), words);
+//			}
+//		});
+		
+	}
 
 	public static void main(String[] args) {
-		Main main = new Main();
-		test4();
-	}
-	static void test3() {
-		Integer data[] = new Integer[] {
-				55, 87, 56, 74, 96, 22, 62, 20, 70, 68, 90, 50
-		};
-		
-		RBTree<Integer> rb = new RBTree<>();
-		for (int i = 0; i < data.length; i++) {
-			rb.add(data[i]);
-			System.out.println("【" + data[i] + "】");
-			BinaryTrees.println(rb);
-			System.out.println("---------------------------------------");
-		}
+		test2();
 	}
 
-	static void test4() {
-		Integer data[] = new Integer[] {
-				55, 87, 56, 74, 96, 22, 62, 20, 70, 68, 90, 50
-		};
-		
-		RBTree<Integer> rb = new RBTree<>();
-		for (int i = 0; i < data.length; i++) {
-			rb.add(data[i]);
-		}
-
-		BinaryTrees.println(rb);
-
-		for (int i = 0; i < data.length; i++) {
-			rb.remove(data[i]);
-			System.out.println("---------------------------------------");
-			System.out.println("【" + data[i] + "】");
-			BinaryTrees.println(rb);
-		}
-	}
 }
--- data-structure-and-algorithm/java/src/com/bestlyg/tree/BinaryTree.java
+++ data-structure-and-algorithm/java/src/com/bestlyg/tree/BinaryTree.java
@@ -201,7 +201,7 @@ public class BinaryTree<E> implements BinaryTreeInfo {
 		/**
 		 * @return 如果返回true，就代表停止遍历
 		 */
-		abstract boolean visit(E element);
+		public abstract boolean visit(E element);
 	}
 	
 	protected static class Node<E> {
