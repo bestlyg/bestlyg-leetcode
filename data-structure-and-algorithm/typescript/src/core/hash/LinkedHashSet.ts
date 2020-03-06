93dd0320d6331b6ef720a84cdc195d88e92d9e1c
bestlyg <1057966749@qq.com>
2020-03-06 11:23:14 +0800

    feat: 🎸 add LinkedHashSet

---
+++ data-structure-and-algorithm/typescript/src/core/hash/LinkedHashSet.ts
@@ -0,0 +1,31 @@
+import ISet from "../set/ISet";
+import IMap from "../map/IMap";
+import LinkedHashMap from "./LinkedHashMap";
+import { IHash } from "../../types";
+/**
+ * 内置LinkedHashMap
+ */
+export default class LinkedHashSet<T extends IHash> implements ISet<T> {
+  private map: IMap<T, null> = new LinkedHashMap<T, null>();
+  size(): number {
+    return this.map.size();
+  }
+  isEmpty(): boolean {
+    return this.map.isEmpty();
+  }
+  clear(): void {
+    this.map.clear();
+  }
+  contains(element: T): boolean {
+    return this.map.containsKey(element);
+  }
+  add(element: T): void {
+    this.map.put(element, null);
+  }
+  remove(element: T): void {
+    this.map.remove(element);
+  }
+  traversal(visitor: (element: T) => boolean): void {
+    this.map.traversal(visitor);
+  }
+}
