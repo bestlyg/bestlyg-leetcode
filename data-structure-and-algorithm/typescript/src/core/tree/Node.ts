597234143fd8e4039f788ba7cecf91d24ab69ef3
bestlyg <1057966749@qq.com>
2020-03-10 15:44:49 +0800

    feat: 🎸 add iteration type

--- data-structure-and-algorithm/typescript/src/core/tree/Node.ts
+++ data-structure-and-algorithm/typescript/src/core/tree/Node.ts
@@ -1,6 +1,7 @@
 import { toString } from "../../utils";
 import Queue from "../queue/Queue";
 import { Visitor, IVisitor, visitorMixin } from "../../utils/visitor_T";
+import Stack from "../stack/Stack";
 export default class Node<T> {
   element: T;
   left: Node<T> | undefined = undefined;
@@ -86,44 +87,108 @@ export function isProper<T>(node: Node<T>): boolean {
  * @param visitor
  * @param node
  */
-export function preorder<T>(visitor: Visitor<T>, node?: Node<T>): void {
-  _preorder(visitorMixin(visitor), node);
+
+//递归
+// export function preorder<T>(visitor: Visitor<T>, node?: Node<T>): void {
+//   _preorder(visitorMixin(visitor), node);
+// }
+// function _preorder<T>(visitor: IVisitor<T>, node?: Node<T>): void  {
+//   if (node === undefined || visitor.stop) return;
+//   visitor.stop = visitor(node.element);
+//   _preorder(visitor, node.left);
+//   _preorder(visitor, node.right);
+// }
+
+//迭代2
+// export function preorder2<T>(visitor: Visitor<T>, node: Node<T>): void {
+//   const stack = new Stack<Node<T>>();
+//   stack.push(node);
+//   let temp: Node<T> | undefined = node;
+//   while (true) {
+//     if (temp !== undefined) {
+//       if (visitor(temp.element)) return;
+//       if (temp.right !== undefined) stack.push(temp.right);
+//       temp = temp.left;
+//     } else if (stack.isEmpty()) {
+//       return;
+//     } else {
+//       temp = stack.pop();
+//     }
+//   }
+// }
+
+//迭代前序遍历
+export function preorder<T>(visitor: Visitor<T>, node: Node<T>): void {
+  const stack = new Stack<Node<T>>();
+  stack.push(node);
+  while (!stack.isEmpty()) {
+    let node = stack.pop();
+    if (visitor(node.element)) return;
+    if (node.right !== undefined) stack.push(node.right);
+    if (node.left !== undefined) stack.push(node.left);
+  }
 }
-function _preorder<T>(visitor: IVisitor<T>, node?: Node<T>) {
-  if (node === undefined || visitor.stop) return;
-  visitor.stop = visitor(node.element);
-  _preorder(visitor, node.left);
-  _preorder(visitor, node.right);
-}
+
 /**
  * 中序遍历
  * @param visitor
  * @param node
  */
-export function inorder<T>(visitor: Visitor<T>, node?: Node<T>): void {
-  _inorder(visitorMixin(visitor), node);
-}
-function _inorder<T>(visitor: IVisitor<T>, node?: Node<T>) {
-  if (node === undefined || visitor.stop) return;
-  _inorder(visitor, node.left);
-  if (visitor.stop) return;
-  visitor.stop = visitor(node.element);
-  _inorder(visitor, node.right);
+// export function inorder<T>(visitor: Visitor<T>, node?: Node<T>): void {
+//   _inorder(visitorMixin(visitor), node);
+// }
+// function _inorder<T>(visitor: IVisitor<T>, node?: Node<T>) {
+//   if (node === undefined || visitor.stop) return;
+//   _inorder(visitor, node.left);
+//   if (visitor.stop) return;
+//   visitor.stop = visitor(node.element);
+//   _inorder(visitor, node.right);
+// }
+export function inorder<T>(visitor: Visitor<T>, node: Node<T>) {
+  const stack = new Stack<Node<T>>();
+  let temp: Node<T> | undefined = node;
+  while (true) {
+    if (temp !== undefined) {
+      stack.push(temp);
+      temp = temp.left;
+    } else if (stack.isEmpty()) {
+      return;
+    } else {
+      temp = stack.pop();
+      if (visitor(temp.element)) return;
+      temp = temp.right;
+    }
+  }
 }
 /**
  * 后序遍历
  * @param visitor
  * @param node
  */
-export function postorder<T>(visitor: Visitor<T>, node?: Node<T>): void {
-  _postorder(visitorMixin(visitor), node);
-}
-function _postorder<T>(visitor: IVisitor<T>, node?: Node<T>) {
-  if (node === undefined || visitor.stop) return;
-  _postorder(visitor, node.left);
-  _postorder(visitor, node.right);
-  if (visitor.stop) return;
-  visitor.stop = visitor(node.element);
+// export function postorder<T>(visitor: Visitor<T>, node?: Node<T>): void {
+//   _postorder(visitorMixin(visitor), node);
+// }
+// function _postorder<T>(visitor: IVisitor<T>, node?: Node<T>) {
+//   if (node === undefined || visitor.stop) return;
+//   _postorder(visitor, node.left);
+//   _postorder(visitor, node.right);
+//   if (visitor.stop) return;
+//   visitor.stop = visitor(node.element);
+// }
+export function postorder<T>(visitor: Visitor<T>, node: Node<T>) {
+  let prev: Node<T> | undefined = undefined;
+  const stack = new Stack<Node<T>>();
+  stack.push(node);
+  while (!stack.isEmpty()) {
+    const top = stack.top();
+    if (top.hasNoChildren() || (prev !== undefined && prev.parent === top)) {
+      prev = stack.pop();
+      if (visitor(prev.element)) return;
+    } else {
+      if (top.right !== undefined) stack.push(top.right);
+      if (top.left !== undefined) stack.push(top.left);
+    }
+  }
 }
 /**
  * 层序遍历
