88f6e0bbd6c749dfc9496d0e8e79877a4b0aa80d
bestlyg <1057966749@qq.com>
2020-05-13 09:46:37 +0800

    feat: 🎸 add leetcode no.102 new solve

--- utils/tree.js
+++ utils/tree.js
@@ -32,12 +32,32 @@ function treeNodeFactory(arr) {
 /**
  * 树的中序遍历
  * @param {TreeNode} node
- * @param {(node:TreeNode)=>void} visitor
+ * @param {(node:TreeNode)=>boolean} visitor
  */
 function treeNodeInorder(node, visitor) {
-  if (node.left !== null) treeNodeInorder(node.left, visitor);
-  visitor(node);
-  if (node.right !== null) treeNodeInorder(node.right, visitor);
+  let f = false;
+  _inorder(node);
+  function _inorder(node) {
+    if (node === null || f) return;
+    _inorder(node.left, visitor);
+    if (f) return;
+    f = visitor(node);
+    _inorder(node.right, visitor);
+  }
+}
+/**
+ * 树的层序遍历
+ * @param {TreeNode} node
+ * @param {(node:TreeNode)=>boolean} visitor
+ */
+function treeNodeLevelOrder(node, visitor) {
+  const queue = [node];
+  while (queue.length !== 0) {
+    const node = queue.shift();
+    if (visitor(node)) return;
+    if (node.left !== null) queue.push(node.left);
+    if (node.right !== null) queue.push(node.right);
+  }
 }
 /**
  * 打印树
@@ -45,7 +65,16 @@ function treeNodeInorder(node, visitor) {
  */
 function treeNodePrint(node) {
   let string = "";
-  treeNodeInorder(node, (node) => (string += node.val + " "));
-  return string.substr(0, string.length - 1);
+  treeNodeInorder(node, (node) => {
+    string += node.val + " ";
+    return false;
+  });
+  console.log(string.substr(0, string.length - 1));
 }
-module.exports = { TreeNode, treeNodeFactory, treeNodeInorder, treeNodePrint };
+module.exports = {
+  TreeNode,
+  treeNodeFactory,
+  treeNodeInorder,
+  treeNodeLevelOrder,
+  treeNodePrint,
+};
