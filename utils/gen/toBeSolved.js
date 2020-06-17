043b13422110c05244a095c97b3e743a34ac373e
bestlyg <1057966749@qq.com>
2020-06-17 21:18:49 +0800

    feat: 🎸 add leetcode no.1300

--- utils/gen/toBeSolved.js
+++ utils/gen/toBeSolved.js
@@ -6,10 +6,5 @@ module.exports = [
   {
     name: "887.鸡蛋掉落",
     url: "https://leetcode-cn.com/problems/super-egg-drop/"
-  },
-  {
-    name: "1300.转变数组后最接近目标值的数组和",
-    url:
-      "https://leetcode-cn.com/problems/sum-of-mutated-array-closest-to-target/"
   }
 ];
