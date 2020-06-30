6c0329cea3a13441a77d63645c41bad601f879eb
bestlyg <1057966749@qq.com>
2020-06-30 14:00:30 +0800

    feat: 🎸 add difficulty index

--- utils/gen/genReadme.js
+++ utils/gen/genReadme.js
@@ -5,6 +5,7 @@ const srcPath = resolve("src");
 const addField = (name, url) => `- [${name}](${url})\n`;
 let res = "# bestlyg-leetcode\n" + "## 介绍\n" + "个人 LeetCode 题解\n";
 const tagReg = new RegExp("标签：(.*)  ");
+const difReg = new RegExp("难度：(.*)  ");
 /**
  * 文档排序规则，若存在特殊字符串与数字比较则数字排前
  */
@@ -36,6 +37,7 @@ function addToBeResolved() {
 function resolveFolder() {
   const indexCache = cache["顺序索引"];
   const tagCache = cache["标签索引"];
+  const difCache = cache["难度索引"];
   const folders = fs.readdirSync(srcPath).sort((a, b) => {
     if (a === "other") return 1;
     else if (b === "other") return -1;
@@ -52,6 +54,12 @@ function resolveFolder() {
       const path = addField(md.substr(0, md.length - 3), `src/${folder}/${md}`);
       indexCache[folder].push(path);
       const file = fs.readFileSync(`${srcPath}/${folder}/${md}`).toString();
+      const difGroup = difReg.exec(file);
+      if (difGroup !== null) {
+        const dif = difGroup[1];
+        difCache[dif] = !difCache[dif] ? [] : difCache[dif];
+        difCache[dif].push(path);
+      }
       const tagGroup = tagReg.exec(file);
       const tags = tagGroup[1].split("、").filter((v) => v !== "");
       for (const tag of tags) {
@@ -60,13 +68,12 @@ function resolveFolder() {
       }
     }
   }
-  for (let [k, v] of Object.entries(indexCache)) indexCache[k] = v.sort(mdSort);
-  for (let [k, v] of Object.entries(tagCache)) tagCache[k] = v.sort(mdSort);
 }
 function toString() {
   for (const key of Object.keys(cache)) {
     res += `## ${key}\n`;
     for (const [k, v] of Object.entries(cache[key])) {
+      cache[key][k] = v.sort(mdSort);
       res += `### ${k}\n`;
       for (const field of v) res += field;
     }
