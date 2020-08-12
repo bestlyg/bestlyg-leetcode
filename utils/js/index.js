d005ce5b6253d17b0827d4be1ded0f0ccf630f5c
bestlyg <1057966749@qq.com>
2020-08-12 09:27:08 +0800

    feat: 🎸 add leetcode no.133

--- utils/js/index.js
+++ utils/js/index.js
@@ -1,6 +1,8 @@
 const tree = require('./tree');
 const list = require('./list');
+const node = require('./node');
 module.exports = {
   ...tree,
   ...list,
+  ...node,
 };
---
+++ utils/js/node.js
@@ -0,0 +1,7 @@
+class Node {
+  constructor(val = 0, neighbors = []) {
+    this.val = val;
+    this.neighbors = neighbors;
+  }
+}
+module.exports = { Node };
