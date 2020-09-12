f88b6dec720655c24b920dd39086561ecb321558
bestlyg <1057966749@qq.com>
2020-09-12 11:56:54 +0800

    feat: 🎸 add leetcode no.637

--- webhooks.js
+++ webhooks.js
@@ -10,30 +10,30 @@ function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
   var child = spawn(cmd, args);
   var resp = '';
-  child.stdout.on('data', function(buffer) {
+  child.stdout.on('data', function (buffer) {
     resp += buffer.toString();
   });
-  child.stdout.on('end', function() {
+  child.stdout.on('end', function () {
     callback(resp);
   });
 }
-handler.on('error', function(err) {
+handler.on('error', function (err) {
   console.error('Error:', err.message);
 });
-handler.on('Push Hook', function(event) {
+handler.on('Push Hook', function (event) {
   console.log(
     'Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref
   );
-  run_cmd('sh', ['./config/deploy.sh'], function(text) {
+  run_cmd('sh', ['./config/deploy.sh'], function (text) {
     console.log(text);
   });
 });
 try {
   http
-    .createServer(function(req, res) {
-      handler(req, res, function(err) {
+    .createServer(function (req, res) {
+      handler(req, res, function (err) {
         res.statusCode = 404;
         res.end('no such location');
       });
